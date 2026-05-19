153a8dc0858b80e2ec1337db50ef5758eafebc8d
mahmood stitia <mahmoodi@mail.tau.ac.il>
2026-05-19 19:37:06 +0300

    first try stopwatch

--- Stopwatch.v
+++ Stopwatch.v
@@ -24,9 +24,9 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Stopwatch(clk, btnC, btnU, btnR, btnL, seg, an, dp, led_left, led_right);
+module Stopwatch(clk, btnC, btnU, btnR, btnL, btnD, seg, an, dp, led_left, led_right);
 
-    input              clk, btnC, btnU, btnR, btnL;
+    input              clk, btnC, btnU, btnR, btnL, btnD;
     output  wire [6:0] seg;
     output  wire [3:0] an;
     output  wire       dp; 
@@ -34,11 +34,119 @@ module Stopwatch(clk, btnC, btnU, btnR, btnL, seg, an, dp, led_left, led_right);
     output  wire [2:0] led_right;
 
     wire [15:0] time_reading;
-    wire trig, split, reset, toggle;
-    wire trig_right, split_right, init_regs_right, count_enabled_right;
-    wire trig_left, split_left, init_regs_left, count_enabled_left;
-    reg selected_stopwatch;
+    wire trig, split, reset, toggle, sample; // button inputs - stable
+    
+//    wire trig_right, split_right, init_regs_right, count_enabled_right;
+//    wire trig_left, split_left, init_regs_left, count_enabled_left;
+    
+    // time values
+    wire [7:0] time_reading_counter;
+    wire [7:0] time_reading_stash;
+
+    reg selected_stopwatch = 1'b0; // 0 for stopWatch, 1 for stash
+    wire init_regs, count_enabled; // control signals
+    
+    //snapshot
+    reg [7:0] snapshot;
+    reg split_active;
+    wire trig_ctl, split_ctl;
+    wire [7:0] counter2display;
+    
     
 	// FILL HERE INSTANTIATIONS 
+	
+	// Internal Assigns
+	assign time_reading =  16'h0000 | {counter2display, time_reading_stash};
+	assign counter2display = split_active? snapshot : time_reading_counter;
+	assign trig_ctl  = (selected_stopwatch == 1'b0) ? trig  : 1'b0;
+	assign split_ctl = (selected_stopwatch == 1'b0) ? split : 1'b0;
+	
+	
+	// Buttons Stabilization
+	Debouncer #(.COUNTER_BITS(20)) debounce_reset(
+	.clk(clk),
+	.input_unstable(btnC),
+	.output_stable(reset)
+	);
+	
+	Debouncer #(.COUNTER_BITS(20)) debounce_trigger(
+	.clk(clk),
+	.input_unstable(btnU),
+	.output_stable(trig)
+	);
+	
+	Debouncer #(.COUNTER_BITS(20)) debounce_split(
+	.clk(clk),
+	.input_unstable(btnR),
+	.output_stable(split)
+	);
+	
+	Debouncer #(.COUNTER_BITS(20)) debounce_toggle(
+	.clk(clk),
+	.input_unstable(btnL),
+	.output_stable(toggle)
+	);
+	
+	Debouncer #(.COUNTER_BITS(20)) debounce_btnD(
+	.clk(clk),
+	.input_unstable(btnD),
+	.output_stable(sample)
+	);
+	
+	Ctl control(
+	.clk(clk), 
+	.reset(reset),
+	.trig(trig_ctl),
+	.split(split_ctl), 
+	.init_regs(init_regs), 
+	.count_enabled(count_enabled)
+	);
+	
+	Counter #(.CLK_FREQ(100000000)) counter(
+	.clk(clk), 
+	.init_regs(init_regs), 
+	.count_enabled(count_enabled), 
+	.time_reading(time_reading_counter)
+	);
+	
+	Stash #(.DEPTH(5)) stash(
+	.clk(clk), 
+	.reset(reset), 
+	.sample_in(time_reading_counter), 
+	.sample_in_valid(sample), 
+	.next_sample(selected_stopwatch? trig : 1'b0), // if in stash mode btnU is next_sample
+	.sample_out(time_reading_stash)
+	);
+	
+	Seg_7_Display seg_7(
+	.x(time_reading),
+	.clk(clk),
+	.clr(reset),
+	.a_to_g(seg),
+	.an(an),
+	.dp(dp)
+	);
+	
+	always @ (posedge clk) begin  // toggle mode: stopWatch/stash
+	 if(reset) selected_stopwatch <= 0;
+	 else if(toggle) selected_stopwatch <= ~selected_stopwatch;
+	end
+	
+	always @(posedge clk) begin
+	 if(reset) begin
+	  split_active <= 1'b0;
+	  snapshot <= 8'h00;
+	 end else begin
+	  if(trig_ctl) split_active <= 1'b0; // PAUSE + trig press
+	  else if(count_enabled && split_ctl) begin 
+	   snapshot <= time_reading_counter;
+	   split_active <= 1'b1;
+	  end
+	  
+	 end
+	end
+	
+	assign led_left  = (selected_stopwatch == 1'b0) ? 3'b111 : 3'b000; // stopwatch
+    assign led_right = (selected_stopwatch == 1'b1) ? 3'b111 : 3'b000; // stash
 
 endmodule
